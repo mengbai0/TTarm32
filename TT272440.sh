d6b0de02760cd4b9f40b7a928a3561361222e839
898811295 <898811295@users.noreply.github.com>
2020-10-28 21:34:55 +0800

    Add files via upload

---
+++ TT272440.sh
@@ -0,0 +1,45 @@
+#PJÐ¡Óî
+#ÌðÌÇÑûÇëÂë£º272440
+
+cd /root
+
+rm -f main.zip
+
+cd /root
+
+rm -rf /root/-TTarm32-main
+
+cd /root
+
+rm -rf /usr/node
+
+cd /.
+
+mkdir mnts /usr/node
+
+cd /root/-TTarm32-main
+
+#cd /root/-arm32-main/waz/tt272440
+
+/bin/cp -rf node/*  /usr/node
+
+cd /usr
+
+chmod -R 777 /usr/node
+
+cd /root/-TTarm32-main
+
+/bin/cp -rf etc/*  /etc
+
+cd /root
+
+rm -f main.zip
+
+cd /root
+
+rm -rf /root/-TTarm32-main
+
+echo -e "\033[31m ºìÉ«×Ö \033[1m"
+echo "Congratulations, Tiantang has been started successfully! New people fill in Tiantang invitation code: 272440 can get 15 bonus cards. Your support is my driving force----@PJxiaoyu"
+echo "QQ:898811295"
+
