07d11c6d181d6d9b9612f6660c344b1ae008915e
898811295 <898811295@users.noreply.github.com>
2020-10-29 12:54:46 +0800

    Update crash_monitor.sh

--- node/crash_monitor.sh
+++ node/crash_monitor.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-
+cd /.
+mount LABEL=272440 /mnts
 d=`date '+%F %T'`;
 num=`ps fax | grep '/ttnode' | egrep -v 'grep|echo|rpm|moni|guard' | wc -l`;
 echo $num;
 if [ $num -lt 1 ];then
  echo "[$d] ttnode is dead...restarting" >> /usr/node/log.log ;
  echo "[$d] ttnode is dead...restarting";
- mount LABEL=272440 /mnts
  /usr/node/ttnode -p /mnts;
 fi
